f91f522e2eff3273e73c62a9add3ecb2b864b463
panterasbook29 <alexturcu2006@gmail.com>
2023-11-17 17:46:31 +0200

    Register/Log In

--- HealthCare2021/Assets/Scripts/AuthManager.cs
+++ HealthCare2021/Assets/Scripts/AuthManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor.VersionControl;
 using UnityEngine.Networking;
 
 public class AuthManager : MonoBehaviour
@@ -11,7 +10,8 @@ public class AuthManager : MonoBehaviour
     public TMP_InputField phoneNumberInput;
     public TMP_InputField passwordInput;
 
-    public Button submitButton;
+    public Button submitButtonRegister;
+    public Button submitButtonLogin;
 
     public void CallRegister()
     {
@@ -39,8 +39,43 @@ public class AuthManager : MonoBehaviour
         }
     }
 
+
+    public void CallLogin()
+    {
+       StartCoroutine(Login());
+    }
+
+    IEnumerator Login()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("phoneNumber", phoneNumberInput.text);
+        form.AddField("password", passwordInput.text);
+
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/login.php", form);
+
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            if (www.downloadHandler.text[0] == '0')
+            {
+                DBManager.username = phoneNumberInput.text;
+                Debug.Log(www.downloadHandler.text); ;
+            }
+            else
+            {
+                Debug.Log("User Login failed with error #" + www.downloadHandler.text);
+            }
+        }
+        else
+        {
+            Debug.LogError("Error: " + www.error);
+        }
+    }
+
     public void VerifyInputs()
     {
-        submitButton.interactable = (phoneNumberInput.text.Length >= 8 && passwordInput.text.Length >= 4) ;
+        submitButtonRegister.interactable = (phoneNumberInput.text.Length >= 8 && passwordInput.text.Length >= 4) ;
+        submitButtonLogin.interactable = (phoneNumberInput.text.Length >= 8 && passwordInput.text.Length >= 4);
     }
 }
---
+++ HealthCare2021/Assets/Scripts/WebTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebTest : MonoBehaviour
+{
+
+    IEnumerator Start()
+    {
+        UnityWebRequest request = UnityWebRequest.Get("http://localhost/sqlconnect/webtest.php");
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string[] webResults = request.downloadHandler.text.Split('\t');
+            foreach(string s in webResults)
+            {
+                Debug.Log(s);
+
+            }
+        }
+        else
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+    }
+}
